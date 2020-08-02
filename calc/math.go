f58acc8adf3e24d65b1f693ec784604fea23364c
Janusz Chludzinski <janusz.chludzinski@polidea.com>
2020-08-02 13:18:31 +0200

    Add() function v2

--- calc/math.go
+++ calc/math.go
@@ -1,12 +1,18 @@
 package calc
 
+import "errors"
+
 // return sum of integers
-func Add(numbers ...int) int {
+func Add(numbers ...int) (error, int) {
 	sum := 0
+
+	if len(numbers) < 2 {
+		return errors.New("Provide at least 2 arguments"), sum
+	}
 
 	for _, num := range numbers {
 		sum += num
 	}
 
-	return sum
+	return nil, sum
 }
